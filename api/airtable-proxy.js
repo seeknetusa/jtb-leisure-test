bf6974bb93b18452f2abad660844d0e2ec17b178
mabe07 <m.abe@us.seeknetusa.com>
2025-07-22 18:15:32 -0700

    update

--- api/airtable-proxy.js
+++ api/airtable-proxy.js
@@ -14,6 +14,8 @@ export default async function handler(req, res) {
   const token = process.env.AIRTABLE_TOKEN;
   const baseId = process.env.AIRTABLE_BASE_ID;
 
+  console.log('table', table);
+  console.log('sortField', sortField);
   /*
   let tableName;
 
