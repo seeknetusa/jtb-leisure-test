af26db9c2b483ec761f4bdcc09ec097b199f5fdd
mabe07 <m.abe@us.seeknetusa.com>
2025-08-12 10:58:50 -0700

    u

--- public/tour.js
+++ public/tour.js
@@ -1669,6 +1669,8 @@ if (window.location.pathname.includes('/contact')) {
 
     try {
       const tours = await fetchTour(tid); // tours は配列
+
+      console.log('tours:', tours);
       if (!Array.isArray(tours) || tours.length === 0) {
         console.warn('Tour データがありません');
         return;
