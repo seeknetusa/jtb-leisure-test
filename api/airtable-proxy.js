dfad4b97d9de7b9f10b30fd334d46149135a49c6
mabe07 <m.abe@us.seeknetusa.com>
2025-08-25 14:39:12 -0700

    test

--- api/airtable-proxy.js
+++ api/airtable-proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   // CORS 対応（Webflowドメインからのアクセス許可）
-  //res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Origin', 'https://jtb-leisure.webflow.io');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  //res.setHeader('Access-Control-Allow-Origin', 'https://jtb-leisure.webflow.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   //res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
