ccf24d2522e69d20e9e6d49986da7b97294887fd
mabe07 <m.abe@us.seeknetusa.com>
2025-09-15 10:54:05 -0700

    up

--- api/airtable-proxy.js
+++ api/airtable-proxy.js
@@ -62,6 +62,8 @@ export default async function handler(req, res) {
     formulas.push(`{Publish}=TRUE()`);
   }
   
+  console.log('TEST');
+
   console.log('tableName2', tableName);
   console.log('filterField2', filterField);
   console.log('filterValue2', filterValue);
